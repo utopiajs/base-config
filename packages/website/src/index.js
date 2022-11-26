627e3ace59f4af3c9d52c9d8da6927c74dd72b95
KuangPF <1633397286@qq.com>
2022-11-26 17:51:40 +0800

    feat(website): test for rules

---
+++ packages/website/src/index.js
@@ -0,0 +1,2 @@
+const a = require('@utopia/eslint-config-react')
+console.log(a)
---
+++ packages/website/src/react/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div>base config</div>;
+}
+
+export default App;
