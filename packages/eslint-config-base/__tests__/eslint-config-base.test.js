8a9c58b8214af47a3b4af38dd9383fb2db831651
KuangPF <1633397286@qq.com>
2022-11-27 10:50:23 +0800

    chore(eslint-config-vue): initialize eslint-config-vue

--- packages/eslint-config-base/__tests__/eslint-config-base.test.js
+++ packages/eslint-config-base/__tests__/eslint-config-base.test.js
@@ -1,7 +1,2 @@
 'use strict';
-
-const eslintConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(eslintConfig(), 'Hello from eslintConfig');
-console.info("eslintConfig tests passed");
+// TODO
